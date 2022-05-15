e3f8980c404c2da6d539d6ef3a91dbc82cb9c135
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2022-05-16 04:08:44 +0530

    Add files via upload

--- src/routes/api/blogs.js
+++ src/routes/api/blogs.js
@@ -1,1 +1,14 @@
+const express = require('express');
 
+const { getBlogs,getBlogsById,postBlog,patchBlog,deleteBlog} = require('../../controllers/api/blogs');
+
+const router = express.Router();
+
+router.get('/api/blogs',getBlogs);//configuring the router
+// "id" is a path parameter
+router.get('/api/blogs/:id',getBlogsById);
+router.post('/api/blogs', postBlog);
+router.patch('/api/blogs/:id',patchBlog);
+router.delete('/api/blogs/:id',deleteBlog);
+
+module.exports = router; //exporting the router
